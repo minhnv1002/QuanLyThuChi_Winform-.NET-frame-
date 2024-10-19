30b61a0b1613f6276007e0c0a2dce671a39dfdde
Van Minh <minhnv10201@gmail.com>
2024-10-19 13:17:34 +0700

    Initial commit

---
+++ QuanLyThuChi/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuChi
+{
+    public partial class close_But : Form
+    {
+        public close_But()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
